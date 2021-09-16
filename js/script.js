22cc40ec4496cb023b885972ace6e93eb66cb5ce
Sadik-Sarker <84263867+Sadik-Sarker@users.noreply.github.com>
2021-09-16 20:22:35 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slick_main').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 2000,
+        vertical: true,
+        arrows: false,
+        infinite: true
+      });
+  });
